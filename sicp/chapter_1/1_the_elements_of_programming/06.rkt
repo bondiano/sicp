9f1784a0de7d0ea55bc6be09c95f84f9aba86eca
bondiano <batbondik0@gmail.com>
2018-05-27 12:26:04 +0700

    add vscode config, solve 07

--- sicp/chapter_1/1_the_elements_of_programming/06.rkt
+++ sicp/chapter_1/1_the_elements_of_programming/06.rkt
@@ -1,8 +1,8 @@
 #lang racket/base
 
 (define (new-if predicate then-clause else-clause)
-    (cond (predicate then-clause)
-        (else else-clause)))
+  (cond (predicate then-clause)
+      (else else-clause)))
 
 (new-if (= 2 3) 0 5)
 ; 5
@@ -11,25 +11,25 @@
 ; 0
 
 (define (average x y)
-    (/ (+ x y) 2))
+  (/ (+ x y) 2))
 
 (define (square x) 
-    (* x x))
+  (* x x))
 
 (define (improve guess x)
-    (average guess (/ x guess)))
+  (average guess (/ x guess)))
 
 (define (good-enough? guess x)
-    (< (abs (- (square guess) x)) 0.001))
+  (< (abs (- (square guess) x)) 0.001))
 
 (define (sqrt-iter guess x)
-    (new-if (good-enough? guess x)
-    guess
-    (sqrt-iter (improve guess x)
-        x)))
+  (new-if (good-enough? guess x)
+  guess
+  (sqrt-iter (improve guess x)
+    x)))
 
 (define (sqrt x)
-    (sqrt-iter 1.0 x))
+  (sqrt-iter 1.0 x))
 
 ; Из-за аппликативного порядка вычислений Lisp интерпретатор сперва попробует вычислить аргументы, перед тем, как выполнить процедуру
 ; (new-if) и тк sqrt-iter рекурсивна, мы уходим в бесконечную рекурсию
--- sicp/chapter_1/1_the_elements_of_programming/07.rkt
+++ sicp/chapter_1/1_the_elements_of_programming/07.rkt
@@ -1,1 +1,48 @@
 #lang racket/base
+
+(define (average x y)
+  (/ (+ x y) 2))
+
+(define (square x) 
+  (* x x))
+
+(define (improve guess x)
+  (average guess (/ x guess)))
+
+(define (good-enough? guess x)
+  (< (abs (- (square guess) x)) 0.001))
+
+(define (sqrt-iter guess x)
+  (if (good-enough? guess x)
+  guess
+  (sqrt-iter (improve guess x)
+    x)))
+
+(define (sqrt x)
+  (sqrt-iter 1.0 x))
+
+; Solution
+
+(define (better-good-enough? guess perv-guess)
+  (< (abs (- guess perv-guess)) 0.001))
+
+(define (better-sqrt-iter guess x perv-guess)
+  (if (better-good-enough? guess perv-guess)
+  guess
+  (better-sqrt-iter (improve guess x)
+    x
+    guess)))
+
+(define (better-sqrt x)
+    (better-sqrt-iter 1.0 x x))
+
+(module+ test
+  (require rackunit)
+  (test-case
+    "1_07"
+      (check-equal? (round (better-sqrt 4)) 2.0)
+      (check-equal? (round (better-sqrt 9)) 3.0)
+      (check-equal? (round (better-sqrt 16)) 4.0)
+      (check-equal? (round (better-sqrt 25)) 5.0)
+  )
+)

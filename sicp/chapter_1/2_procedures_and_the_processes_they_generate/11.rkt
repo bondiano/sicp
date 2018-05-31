d2210682543534680d687e5f552fae8f1fd0380a
bondiano <batbondik0@gmail.com>
2018-05-31 13:08:10 +0700

    Solve 1_11 and 1_12

---
+++ sicp/chapter_1/2_procedures_and_the_processes_they_generate/11.rkt
@@ -0,0 +1,31 @@
+#lang racket/base
+
+(define (f-rec n)
+    (if (< n 3) 
+        n
+        (+ (f-rec (- n 1)) (f-rec (- n 2)) (f-rec (- n  3)))))
+
+(define (f-iter n)
+    (define (iter last perv perv-perv counter)
+        (if (= counter n)
+            last
+            (iter (+ last perv perv-perv) last perv (+ counter 1))))
+    (if (< n 3) 
+        n
+        (iter 3 2 1 3)))
+
+
+(module+ test
+  (require rackunit)
+  (test-case
+    "1_11"
+    (check-equal? (f-rec 0) 0)
+    (check-equal? (f-rec 2) 2)
+    (check-equal? (f-rec 3) 3)
+    (check-equal? (f-rec 4) 6)
+    (check-equal? (f-rec 1) (f-iter 1))
+    (check-equal? (f-rec 5) (f-iter 5))
+    (check-equal? (f-rec 8) (f-iter 8))
+    (check-equal? (f-rec 10) (f-iter 10))
+  )
+)
